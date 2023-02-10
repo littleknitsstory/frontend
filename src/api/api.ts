41eadab57cd2bd5b93d4711715aca49d2c103d71
Denis Karpenko <denis_swd@mail.ru>
2023-02-10 18:53:16 +0100

    feat: api types

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,8 @@
 import {CategoriesType, ProductsType, ReviewsType} from "../mainTypes";
 
 const baseUrl = 'http://dev.backend.littleknitsstory.com:26363/api/v1/'
-const data = (res: Response) => res.json()
-export const api = (url: string, init?: any) => fetch(new URL(url, baseUrl), init)
-export const getCategories = (): Promise<CategoriesType> => api('categories').then(data)
-export const getReviews = (): Promise<ReviewsType> => api('reviews').then(data)
-export const getProducts = (): Promise<ProductsType> => api('products/?limit=10&offset=0').then(data)
+const data = <T extends object>(res: Response): Promise<T> => res.json()
+export const api = (url: string, init?: RequestInit) => fetch(new URL(url, baseUrl), init)
+export const getCategories = () => api('categories').then(data<CategoriesType>)
+export const getReviews = () => api('reviews').then(data<ReviewsType>)
+export const getProducts = () => api('products/?limit=10&offset=0').then(data<ProductsType>)
