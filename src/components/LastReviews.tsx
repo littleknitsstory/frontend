57c7c75138e28eecdac4b2243caad443804091db
Denis Karpenko <denis_swd@mail.ru>
2023-02-10 18:59:47 +0100

    feat: last rewviews type, use effect dependency

--- src/components/LastReviews.tsx
+++ src/components/LastReviews.tsx
@@ -1,25 +1,25 @@
-import React, {useEffect} from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React, {FC, useEffect} from "react";
+import {Container} from "react-bootstrap";
 import CardReview from "./CardReview";
 import {useAppDispatch, useAppSelector} from "../store/hooks";
 import {getReviewsThunk, selectReviews} from "../store/apiTestSlice";
 
-const LastReviews = () => {
-    const dispatch = useAppDispatch()
-    const reviews = useAppSelector(selectReviews)
+const LastReviews: FC = () => {
+  const dispatch = useAppDispatch()
+  const reviews = useAppSelector(selectReviews)
 
-    useEffect(() => {
-        dispatch(getReviewsThunk())
-    }, [])
+  useEffect(() => {
+    dispatch(getReviewsThunk())
+  }, [dispatch])
 
-    return (
+  return (
     <section className="last-reviews">
       <Container>
         <h3 className="title">Последние отзывы</h3>
-          {reviews.map((r, i) => <CardReview key={i}
-                                             title={r.title}
-                                             authorName={r.author}
-                                             description={r.comment}/>)}
+        {reviews.map((r, i) => <CardReview key={i}
+                                           title={r.title}
+                                           authorName={r.author}
+                                           description={r.comment}/>)}
       </Container>
     </section>
   );
