d60c0208f3a4fcf120de14b4ec7f46bd0ade8a00
chekolovec <chekolovec@gmail.com>
2023-02-22 21:06:49 +0100

    add products request to store

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,41 +1,23 @@
-import { useCallback, useContext, useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 
-import { LanguageContext } from "../App";
-import { getProducts } from "../api";
-import { IProductsResponse } from "../api/models";
 import arrowRight from "../icons/arrow-right.svg";
-import { IProduct } from "../store/productSlice";
+import { useGetProductsQuery } from "../services";
 import CardProduct from "./CardProduct";
 import Filters from "./Filters";
+import Spinner from "./Spinner";
 
 const Products = () => {
   const { t } = useTranslation();
-  const { language } = useContext(LanguageContext);
 
-  const [products, setProducts] = useState<IProduct[]>([]);
-  const [limit, setLimit] = useState<number>(0);
-  const [count, setCount] = useState<number>(0);
-  const isLastPage = limit + 4 >= count;
+  const [limit, setLimit] = useState<number>(4);
 
-  useEffect(() => {
-    const fetchProducts = async (): Promise<void> => {
-      const data: IProductsResponse | void = await getProducts(0, limit);
-      if (data) {
-        setProducts(data.results);
-        setCount(data.count);
-      }
-    };
-    fetchProducts();
-  }, [limit, language]);
+  const { data, isLoading } = useGetProductsQuery({ limit, offset: 0 });
+  const isLastPage = data?.count ? limit >= data.count : true;
 
   const handleSeeMore = useCallback((): void => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
     setLimit((prev) => prev + 4);
   }, []);
 
@@ -73,19 +55,21 @@ const Products = () => {
           </Col>
           <Col>
             <Row xs={1} md={1} lg={2} xl={2} xxl={3}>
-              {products.map((item) => {
-                return (
-                  <Col key={item.id}>
-                    <CardProduct product={item} />
-                  </Col>
-                );
-              })}
+              {isLoading && <Spinner />}
+              {data &&
+                data.results.map((item) => {
+                  return (
+                    <Col key={item.id}>
+                      <CardProduct product={item} />
+                    </Col>
+                  );
+                })}
             </Row>
           </Col>
         </Row>
         {!isLastPage && (
           <Row>
-            <Col>
+            <Col className="text-end">
               <Link to="/shop" onClick={handleSeeMore}>
                 {t("seeMore")}
                 <img src={arrowRight} alt="arrowRight" />
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+import { PICTURE_BASE_URL } from "../api";
 import { IProductsResponse } from "../api/models";
 
 export const productsAPI = createApi({
@@ -8,8 +9,24 @@ export const productsAPI = createApi({
     baseUrl: "http://dev.backend.littleknitsstory.com:26363",
   }),
   endpoints: (builder) => ({
-    getProducts: builder.query<IProductsResponse, void>({
-      query: () => "api/v1/products/",
+    getProducts: builder.query<
+      IProductsResponse,
+      {
+        offset: number;
+        limit: number;
+      }
+    >({
+      query: ({ offset, limit }) =>
+        `api/v1/products/?offset=${offset}&limit=${limit}`,
+      transformResponse: (response: IProductsResponse) => {
+        return {
+          ...response,
+          results: response.results.map((item) => ({
+            ...item,
+            image_preview: `${PICTURE_BASE_URL}${item.image_preview}`,
+          })),
+        };
+      },
     }),
   }),
 });
