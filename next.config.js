b32d016ef6d764617fb5c5c7a19d9fb9542ea9b0
Aleksandr Trofimov <trofimovalexandr88@gmail.com>
2023-05-16 09:51:29 +0200

    init next-app-router

--- next.config.js
+++ next.config.js
@@ -1,23 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    API_BASE_URL: process.env.API_BASE_URL,
-  },
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en", "ru"],
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "dev.backend.littleknitsstory.com",
-        port: "26363",
-        pathname: "/**",
-      },
-    ],
-  },
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
