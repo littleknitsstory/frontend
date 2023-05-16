84262e55dd8c850209058710036d658826827381
Aleksandr Trofimov <trofimovalexandr88@gmail.com>
2023-05-16 12:23:07 +0200

    add footer template

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,11 +1,13 @@
 import Script from "next/script";
-import { Locale, i18n } from "@/i18n-config";
 import { Montserrat } from "next/font/google";
+
+import { Locale, i18n } from "@/i18n-config";
+import { getDictionary } from "@/get-dictionaries";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "@/styles/globals.scss";
-import Header from "@/components/Header";
-import { getDictionary } from "@/get-dictionaries";
 
 const montserrat = Montserrat({ subsets: ["latin", "cyrillic"] });
 
@@ -32,9 +34,10 @@ export default async function RootLayout({
   return (
     <html lang={params.lang}>
       <body className={montserrat.className}>
-        <div className="container-lg">
+        <div className="container-lg min-vh-100 d-flex flex-column">
           <Header dictionary={dictionary.header} />
           {children}
+          <Footer />
           <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" />
         </div>
       </body>
