374397efd6dbe2b021a373b514aee8e0100417d6
bulochkabu <nastyaerm@mail.ru>
2023-05-26 21:25:58 +0200

    separeted DetailAricle

--- src/app/[lang]/articles/[slug]/page.tsx
+++ src/app/[lang]/articles/[slug]/page.tsx
@@ -13,6 +13,7 @@ import { Suspense } from "react";
 import Spinner from "@/components/utils/Spinner";
 import ReadMoreArticles from "@/components/read-more-articles/ReadMoreArticles";
 import ButtonBack from "@/components/button-back/ButtonBack";
+import CommentsList from "@/components/comments/CommentsList";
 
 interface paramsProps {
   slug: string;
@@ -43,13 +44,18 @@ export default async function Article({ params }: { params: paramsProps }) {
     <>
       <ButtonBack back={dictionary.back} />
       <Suspense fallback={<Spinner />}>
-        <ArticleDetail
-          article={article}
+        {features.blog && (
+          <ArticleDetail
+            article={article}
+            dictionary={dictionary}
+            lang={lang}
+          />
+        )}
+
+        <CommentsList
+          comments={comments}
           features={features}
           dictionary={dictionary}
-          lang={lang}
-          articles={articles}
-          comments={comments}
         />
         <ReadMoreArticles articles={articles} dictionary={dictionary} />
       </Suspense>
--- src/components/article/ArticleDetail.tsx
+++ src/components/article/ArticleDetail.tsx
@@ -1,9 +1,7 @@
-"use client";
-
 import Image from "next/image";
 import { Locale } from "@/i18n-config";
 
-import { Article, CommentsData, FeaturesFlags } from "@/services/types";
+import { Article } from "@/services/types";
 import {
   getLocaleDate,
   getDisplayedName,
@@ -11,7 +9,6 @@ import {
 } from "@/helpers/utils";
 
 import Bookmark from "../bookmark/Bookmark";
-import CommentsList from "../comments/CommentsList";
 import PopoverOverlay from "../popover-overlay/PopoverOverlay";
 
 import tempAvatar from "@/assets/temp-avatar.png";
@@ -23,11 +20,8 @@ import shareIconGrey from "@/assets/icons/reactions/share.svg";
 
 interface Props {
   article: Article;
-  features: FeaturesFlags;
   lang: Locale;
-  articles: Article[];
   dictionary: Dictionary;
-  comments: CommentsData[];
 }
 
 interface Dictionary {
@@ -50,84 +44,68 @@ interface Dictionary {
   };
 }
 
-export default function ArticleDetail({
-  article,
-  features,
-  dictionary,
-  lang,
-  articles,
-  comments,
-}: Props) {
+export default function ArticleDetail({ article, dictionary, lang }: Props) {
   return (
-    <>
-      {features.blog && (
-        <section className="container-lg p-0">
-          <div
-            className="d-flex flex-column p-3 p-md-5 mt-4 w-100"
-            style={getStyleBackground(article.image_preview)}
-          >
-            <div className="d-flex align-items-center gap-2">
-              <h2 className="me-auto text text--md text--bold text--white my-0">
-                {article.title}
-              </h2>
-              {/* <Bookmark /> */}
-              <PopoverOverlay icon={shareIcon} dictionary={dictionary} />
-            </div>
-            <div className="d-flex align-items-center gap-3 mt-2">
-              <Image
-                src={tempAvatar}
-                alt="Avatar"
-                className="rounded-circle"
-                width={50}
-                height={50}
-              />
-              <p className="text text--white m-0">
-                {getDisplayedName(article.author)}
-              </p>
-            </div>
-
-            <div className="d-flex gap-3 mt-3">
-              <p className="text--white">
-                {getLocaleDate(article.created_at, lang)}
-              </p>
-              <p className="text--white">&bull;</p>
-              <p className="text--white">
-                {article.time_for_read} {dictionary.article.tempRead}
-              </p>
-            </div>
-            <div className="d-flex gap-4 mt-auto">
-              {article.tags.map((tag) => (
-                <p key={tag.slug} className="text--white">
-                  #{tag.title}
-                </p>
-              ))}
-            </div>
-          </div>
-          <div className="container-lg mt-4">{article.content}</div>comments
-          <div className="post__footer container-lg">
-            <div className=" post__reactions d-flex gap-5 mt-3 ">
-              <div className="d-flex flex-column align-items-center">
-                <Image src={handIcon} alt="handIcon" />
-                <p className="m-0 ">50</p>
-              </div>
-              <div className="d-flex flex-column align-items-center">
-                <Image src={heartIcon} alt="heartIcon" />
-                <p className="m-0">23</p>
-              </div>
-              <div className="d-flex flex-column align-items-center">
-                <Image src={speechBubbleIcon} alt="speechBubbleIcon" />
-                <p className="m-0">32</p>
-              </div>
-              <PopoverOverlay icon={shareIconGrey} dictionary={dictionary} />
-            </div>
-          </div>
-          <CommentsList
-            comments={comments}
-            features={features}
-            dictionary={dictionary}
+    <section className="container-lg p-0">
+      <div
+        className="d-flex flex-column p-3 p-md-5 mt-4 w-100"
+        style={getStyleBackground(article.image_preview)}
+      >
+        <div className="d-flex align-items-center gap-2">
+          <h2 className="me-auto text text--md text--bold text--white my-0">
+            {article.title}
+          </h2>
+          {/* <Bookmark /> */}
+          <PopoverOverlay icon={shareIcon} dictionary={dictionary} />
+        </div>
+        <div className="d-flex align-items-center gap-3 mt-2">
+          <Image
+            src={tempAvatar}
+            alt="Avatar"
+            className="rounded-circle"
+            width={50}
+            height={50}
           />
-        </section>
-      )}
-    </>
+          <p className="text text--white m-0">
+            {getDisplayedName(article.author)}
+          </p>
+        </div>
+
+        <div className="d-flex gap-3 mt-3">
+          <p className="text--white">
+            {getLocaleDate(article.created_at, lang)}
+          </p>
+          <p className="text--white">&bull;</p>
+          <p className="text--white">
+            {article.time_for_read} {dictionary.article.tempRead}
+          </p>
+        </div>
+        <div className="d-flex gap-4 mt-auto">
+          {article.tags.map((tag) => (
+            <p key={tag.slug} className="text--white">
+              #{tag.title}
+            </p>
+          ))}
+        </div>
+      </div>
+      <div className="container-lg mt-4">{article.content}</div>comments
+      <div className="post__footer container-lg">
+        <div className=" post__reactions d-flex gap-5 mt-3 ">
+          <div className="d-flex flex-column align-items-center">
+            <Image src={handIcon} alt="handIcon" />
+            <p className="m-0 ">50</p>
+          </div>
+          <div className="d-flex flex-column align-items-center">
+            <Image src={heartIcon} alt="heartIcon" />
+            <p className="m-0">23</p>
+          </div>
+          <div className="d-flex flex-column align-items-center">
+            <Image src={speechBubbleIcon} alt="speechBubbleIcon" />
+            <p className="m-0">32</p>
+          </div>
+          <PopoverOverlay icon={shareIconGrey} dictionary={dictionary} />
+        </div>
+      </div>
+    </section>
   );
 }
