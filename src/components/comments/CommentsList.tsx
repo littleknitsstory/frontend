fd945e6f34eaf1bccdebe57247cbca22087c7908
Aleksandr Trofimov <trofimovalexandr88@gmail.com>
2023-06-02 17:38:55 +0200

    clear consolelog

--- src/components/comments/CommentsList.tsx
+++ src/components/comments/CommentsList.tsx
@@ -42,8 +42,6 @@ export default function CommentsList({ dictionary }: Props) {
     fetcher
   );
 
-  console.log(comments);
-
   const handleChange = (e: ChangeEvent<HTMLTextAreaElement>): void => {
     setMessage(e.currentTarget.value);
   };
